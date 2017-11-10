88dd97742a0275c18f524270d5d875a590aedb55
Tomas Tulka <tomas.tulka@gmail.com>
2017-11-10 19:06:20 +0100

    RecexpSyntaxException added

---
+++ src/main/java/cz/net21/ttulka/recexp/RecexpSyntaxException.java
@@ -0,0 +1,11 @@
+package cz.net21.ttulka.recexp;
+
+/**
+ * @author ttulka
+ */
+public class RecexpSyntaxException extends RecexpException {
+
+    protected RecexpSyntaxException(String description) {
+        super(description);
+    }
+}
